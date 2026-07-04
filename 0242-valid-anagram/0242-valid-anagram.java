b3295d5eae57c81fbca1730bbfc772b9a9f6ca96
Manjesh Yadav <manjeshy484@gmail.com>
2026-07-04 13:07:04 +0530

    Time: 18 ms (13.64%), Space: 46.7 MB (9.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -26,19 +26,19 @@ class Solution {
 
         if (s.length() != t.length()) {
             return false;
-        } else {
-            for (int i = 0; i < s.length(); i++) {
-                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
 
-            for (int i = 0; i < t.length(); i++) {
-                if (map.getOrDefault(t.charAt(i), 0) == 0) {
-                    return false;
-                }else{
-                    map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
-                }
+        for (int i = 0; i < t.length(); i++) {
+            if (map.getOrDefault(t.charAt(i), 0) == 0) {
+                return false;
+            }else{
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
             }
-        } 
+        }
+        
         return true;
     }
 }
