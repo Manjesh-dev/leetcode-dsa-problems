ea4b8da4654d9064fd0eff0e5eaf9d4acc4e9d1c
Manjesh Yadav <manjeshy484@gmail.com>
2026-07-09 21:32:11 +0530

    Time: 1 ms (62.53%), Space: 43.7 MB (5.14%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,92 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+}
+
+
+
+class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Insert a word into Trie
+    public void insert(String word) {
+
+        TrieNode current = root;
+
+        for (char ch : word.toCharArray()) {
+
+            int index = ch - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    // Find Longest Common Prefix
+    public String longestPrefix() {
+
+        StringBuilder ans = new StringBuilder();
+
+        TrieNode current = root;
+
+        while (true) {
+
+            int count = 0;
+            int childIndex = -1;
+
+            // Count how many children exist
+            for (int i = 0; i < 26; i++) {
+
+                if (current.children[i] != null) {
+                    count++;
+                    childIndex = i;
+                }
+            }
+
+            // Stop if more than one child or word ends here
+            if (count != 1 || current.isEnd) {
+                break;
+            }
+
+            ans.append((char) ('a' + childIndex));
+
+            current = current.children[childIndex];
+        }
+
+        return ans.toString();
+    }
+}
+
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        Trie trie = new Trie();
+
+        for (String word : strs) {
+            trie.insert(word);
+        }
+
+        return trie.longestPrefix();
+    }
+}
+
