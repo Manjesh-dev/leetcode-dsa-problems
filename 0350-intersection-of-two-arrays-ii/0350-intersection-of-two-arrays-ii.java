b7590241aec4e03074de431c6ed6904e5c61a5ba
Manjesh Yadav <manjeshy484@gmail.com>
2026-07-29 13:21:08 +0530

    Time: 3 ms (95.31%), Space: 44.5 MB (94.14%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,23 +1,42 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        HashMap <Integer, Integer> map1 = new HashMap<>();
-        
-        for (int i : nums1) {
-            map1.put(i, map1.getOrDefault(i,0)+1);   
-        }
-        for (int i = 0; i < nums2.length; i++) {
-            if(map1.getOrDefault(nums2[i], 0) > 0){
-                ans.add(nums2[i]);
-                map1.put(nums2[i], map1.get(nums2[i])-1); 
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        if (nums1.length > nums2.length) {
+            for (int i = 0; i < nums2.length; i++) {
+                map.put(nums2[i], map.getOrDefault(nums2[i], 0) + 1);
+            }
+            for (int i = 0; i < nums1.length; i++) {
+                if (map.containsKey(nums1[i])) {
+                    list.add(nums1[i]);
+                    map.put(nums1[i], map.getOrDefault(nums1[i], 0) - 1);
+                    if (map.get(nums1[i]) == 0) {
+                        map.remove(nums1[i]);
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < nums1.length; i++) {
+                map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+            }
+            for (int i = 0; i < nums2.length; i++) {
+                if (map.containsKey(nums2[i])) {
+                    map.put(nums2[i], map.getOrDefault(nums2[i], 0) - 1);
+                    if (map.get(nums2[i]) == 0) {
+                        map.remove(nums2[i]);
+                    }
+                    list.add(nums2[i]);
+                }
             }
         }
-        int[] result = new int[ans.size()];
+        int[] ans = new int[list.size()];
 
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
         }
+        return ans;
 
-        return result; 
+        
     }
 }
