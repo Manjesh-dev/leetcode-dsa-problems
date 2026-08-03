a1027571eaa6fff69c6b166179d5cd5fb0d15e20
Manjesh Yadav <manjeshy484@gmail.com>
2026-08-03 14:36:22 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.37%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -21,7 +21,7 @@ public class Solution extends GuessGame {
                 return guess;
             }
             if (res == -1) {   
-                r = guess;
+                r = guess-1;
             }else{
                 l = guess+1;
             }
