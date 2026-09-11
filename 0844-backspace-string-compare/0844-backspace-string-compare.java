ab5e47ec3c2023bc4fcdb49cc689be89fa5d571f
Manjesh Yadav <manjeshy484@gmail.com>
2026-09-11 20:59:09 +0530

    Time: 4 ms (5.42%), Space: 43.5 MB (14.44%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -3,25 +3,24 @@ class Solution {
         Stack<Character> st1 = new Stack<>();
         Stack<Character> st2 = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '#' && !st1.empty()) {
+        for (int i = 0; i < s.length()+t.length(); i++) {
+            if (s.length()>i && s.charAt(i) == '#' && !st1.empty()) {
                 st1.pop();
             }
-            if (s.charAt(i) != '#') {
+            if (s.length()>i && s.charAt(i) != '#') {
                 st1.push(s.charAt(i));
-            }   
-                
-            
-        }
-        for (int i = 0; i < t.length(); i++) {
-            if (t.charAt(i) == '#' && !st2.empty()) {
+            }      
+            if (t.length()>i && t.charAt(i) == '#' && !st2.empty()) {
                 st2.pop();
             }
-            if (t.charAt(i) != '#') {
+            if (t.length()>i && t.charAt(i) != '#') {
                 
                 st2.push(t.charAt(i));
             } 
+            
         }
+        // for (int i = 0; i < t.length(); i++) {
+        // }
         if (st1.equals(st2)) {
             return true;
         }
