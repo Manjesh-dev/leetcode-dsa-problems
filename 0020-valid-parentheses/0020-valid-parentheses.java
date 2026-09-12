4f5f52b1709c1893b01dbcbff495a2134d948ba5
Manjesh Yadav <manjeshy484@gmail.com>
2026-09-12 05:36:55 +0530

    Time: 6 ms (5.3%), Space: 43.9 MB (5.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,59 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-        // Stack<Character> st = new Stack<>();
-        // if (s.length()%2 !=0) {
-        //     return false;
-        // }
-
-        // int c = 0;
-        // for (int i = 0; i < s.length(); i++) {
-        //     if ((!st.empty()) && s.charAt(i) == ')' && st.peek() == '(') {
-        //         st.pop();
-        //     }
-        //     if ((!st.empty()) && s.charAt(i) == '}' && st.peek() == '{') {
-        //         st.pop();
-        //     }
-        //     if ((!st.empty()) && s.charAt(i) == ']' && st.peek() == '[') {
-        //         st.pop();
-        //     }
-        //     if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-        //         c++;
-        //         st.push(s.charAt(i));
-        //     }
-        // }
-
-        // if (st.size() == 0 && c == s.length() / 2) {
-        //     return true;
-        // }
-        // return false;
-
         Stack<Character> st = new Stack<>();
-
-        if (s.length() % 2 != 0) {
+        if (s.length()%2 !=0) {
             return false;
         }
 
         int c = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (!st.empty()) {
-                if (s.charAt(i) == ')' && st.peek() == '(') {
-                    st.pop();
-                } else if (s.charAt(i) == '}' && st.peek() == '{') {
-                    st.pop();
-                } else if(s.charAt(i) == ']' && st.peek() == '[') {
-                    st.pop();
-                }
+            if ((!st.empty()) && s.charAt(i) == ')' && st.peek() == '(') {
+                st.pop();
+            }
+            if ((!st.empty()) && s.charAt(i) == '}' && st.peek() == '{') {
+                st.pop();
+            }
+            if ((!st.empty()) && s.charAt(i) == ']' && st.peek() == '[') {
+                st.pop();
             }
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 c++;
                 st.push(s.charAt(i));
             }
-
         }
 
         if (st.size() == 0 && c == s.length() / 2) {
             return true;
         }
         return false;
+
     }
 }
